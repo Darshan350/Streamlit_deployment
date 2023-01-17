863f65a9161fceea8c8ce67e91927e3e72ca488c
Darshan350 <57284146+Darshan350@users.noreply.github.com>
2023-01-17 12:05:07 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,6 +1,6 @@
 import pycaret
 #from pycaret import *
-#from pycaret.classification import load_model, predict_model
+from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
 import numpy as np
